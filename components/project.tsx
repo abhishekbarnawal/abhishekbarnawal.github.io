c65be87c4c8051557f967d019a1356d695730661
Abhishek Barnawal <abhishekbarnwal37@gmail.com>
2026-09-16 01:15:51 +0545

    Fix projects data import

--- components/project.tsx
+++ components/project.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useRef } from "react";
-import { projectsData } from "@/lib/data";
+import { projects } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-type ProjectProps = (typeof projectsData)[number];
+type ProjectProps = (typeof projects)[number];
 
 export default function Project({
   title,
